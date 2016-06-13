bdb830ff16a80b8542678135b2eab5352fa03dab
Jeremy Pry <jeremy.pry@gmail.com>
2016-06-12 20:23:22 -0400

    PSR-2 all the things

--- tests/TestData/Namespace.php
+++ tests/TestData/Namespace.php
@@ -1,39 +1,48 @@
 <?php
 namespace Foo {
 
-class A {
-	
-}
+    class A
+    {
 
-class B {
-	public $a;
-	
-	public function __construct(A $a) {
-		$this->a = $a;	
-	}
-}
+    }
 
-class ExtendedA extends A {
-	
-}
+    class B
+    {
+        public $a;
+
+        public function __construct(A $a)
+        {
+            $this->a = $a;
+        }
+    }
+
+    class ExtendedA extends A
+    {
+
+    }
 
 
-class C {
-	public $a;
-		
-	public function __construct(\Bar\A $a) {
-		$this->a = $a;
-	}
-}
+    class C
+    {
+        public $a;
+
+        public function __construct(\Bar\A $a)
+        {
+            $this->a = $a;
+        }
+    }
 
 }
 
 namespace Bar {
-	class A {
-		
-	}	
-	
-	class B {
-		
-	}
+
+    class A
+    {
+
+    }
+
+    class B
+    {
+
+    }
 }
